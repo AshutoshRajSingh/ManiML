a38ce1d4aea2ca287d935c17b0da3f06c9227234
finite-simple-group-of-order-two <ashutosh200210@outlook.com>
2022-11-30 20:00:03 +0530

    BatchGradientDescentOptimizer now implements WBGradientDescentOptimizer

--- linreg/batchgd.py
+++ linreg/batchgd.py
@@ -1,46 +1,15 @@
 import numpy as np
+from optimizer import WBGradientDescentOptimizer
 
-class BatchGradientDescentOptimizer:
-    def __init__(self, lr=0.01) -> None:
-        self._lr = lr
-        self._theta = np.empty([])
-
-    def _compute_mse_gradient(self, x, y, theta):
+class BatchGradientDescentOptimizer(WBGradientDescentOptimizer):
+    def __init__(self, lr) -> None:
+        super().__init__(lr)
+    def compute_gradients(self, x, y, epoch):
         m, _ = x.shape
-        return 2 / m * x.T.dot(x.dot(theta) - y)
-    
-    def _apply_gradient(self, gradient):
-        self._theta = self._theta - gradient * self._lr
-    
-    def _initialize_theta_dims(self, n):
-        self._theta = np.random.rand(n, 1)
 
-    def _perform_training_step(self, x, y):
-        grad = self._compute_mse_gradient(x, y, self._theta)
-        self._apply_gradient(grad)
+        dw = 2 / m * np.sum((x.dot(self._weights) + self._bias - y) * x, axis=0)
+        dw = dw[:, np.newaxis]
     
-    def _initialize_optimizer(self, x, y):
-        try:
-            _, n = x.shape
-            self._initialize_theta_dims(n + 1)
-        except ValueError:
-            raise ValueError('x must be 2-dimensional')
-        
-        if y.shape[-1] != 1 or len(y.shape) != 2:
-            raise ValueError(f'y must be of shape [None, 1], got {y.shape} instead')
-    
-    def _add_bias_term_to_batch(self, x):
-        _, n = x.shape
-        return np.c_[np.ones([n, 1]), x]
+        db = 2 / m * np.sum(x.dot(self._weights) + self._bias - y, axis=0)
 
-    def fit_remembering_weights(self, x, y, epochs=100):
-        self._initialize_optimizer(x, y)
-        x = self._add_bias_term_to_batch(x)
-
-        thetas = list()
-
-        for _ in range(epochs):
-            self._perform_training_step(x, y)
-            thetas.append(self._theta)
-        
-        return thetas
+        return dw, db
