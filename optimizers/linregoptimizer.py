64aa8902840484f0edd276cbfe72ce77b426c9e2
finite-simple-group-of-order-two <ashutosh200210@outlook.com>
2022-12-05 11:09:06 +0530

    rename and use SGDMixin

--- optimizers/linregoptimizer.py
+++ optimizers/linregoptimizer.py
@@ -1,5 +1,5 @@
 import numpy as np
-from .__baseoptimizer import WBGradientDescentOptimizer
+from .__baseoptimizer import WBGradientDescentOptimizer, SGDMixin
 
 
 class BatchGradientDescentOptimizer(WBGradientDescentOptimizer):
@@ -9,7 +9,7 @@ class BatchGradientDescentOptimizer(WBGradientDescentOptimizer):
     def compute_loss(self, x, y):
         return np.mean((x.dot(self._weights) + self._bias - y) ** 2)
 
-    def compute_gradients(self, x, y):
+    def compute_gradients(self, x, y): 
         m, _ = x.shape
 
         dw = 2 / m * np.sum((x.dot(self._weights) +
@@ -21,18 +21,6 @@ class BatchGradientDescentOptimizer(WBGradientDescentOptimizer):
         return dw, db
 
 
-class StochasticGradientDescentOptimizer(BatchGradientDescentOptimizer):
+class StochasticGradientDescentOptimizer(SGDMixin, BatchGradientDescentOptimizer):
     def __init__(self, lr) -> None:
         super().__init__(lr)
-
-    def perform_training_step(self, x, y, epoch):
-        m, _ = x.shape
-
-        random_idx = np.random.randint(0, m)
-
-        x_random = x[random_idx][np.newaxis, :]
-        y_random = y[random_idx][np.newaxis, :]
-
-        dw, db = self.compute_gradients(x_random, y_random)
-
-        self._apply_gradients(dw, db)
