ace10f3f7c40211a0baf19901443411cb4e5a183
finite-simple-group-of-order-two <ashutosh200210@outlook.com>
2022-12-05 11:09:33 +0530

    add sigmoid fn

--- util.py
+++ util.py
@@ -8,3 +8,6 @@ def generate_dummy_linear_data(n=100, w=3, b=4):
 
 def mse_loss(x, y, weights, bias):
     return np.mean((x.dot(weights) + bias - y) ** 2)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
