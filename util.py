87ee736bfcda5cdfc7fa5e0a2014cc9067d243a0
finite-simple-group-of-order-two <ashutosh200210@outlook.com>
2022-12-05 19:07:44 +0530

    implement obtain_classification_data fn

--- util.py
+++ util.py
@@ -1,10 +1,30 @@
 import numpy as np
+from sklearn.datasets import load_iris
 
 def generate_dummy_linear_data(n=100, w=3, b=4):
     x = np.random.rand(n, 1)
     y = b + w * x + np.random.rand(n, 1)
 
     return x, y
+
+def obtain_classification_data():
+    iris = load_iris()
+    petal_dims_setosa = iris.data[:, 2:4][iris.target == 0]
+    petal_dims_virginica = iris.data[:, 2:4][iris.target == 2]
+
+    petal_dims = np.concatenate([petal_dims_setosa, petal_dims_virginica], axis=0)
+
+    petal_labels = np.concatenate([iris.target[iris.target == 0], iris.target[iris.target == 2]])
+
+    petal_data = np.concatenate([petal_dims, petal_labels.reshape(100, 1)], axis=1)
+    petal_data[50:, 2] = 1
+
+    np.random.shuffle(petal_data)
+
+    petals_x = petal_data[:, :-1]
+    petals_y = petal_data[:, -1:]
+
+    return petals_x, petals_y
 
 def mse_loss(x, y, weights, bias):
     return np.mean((x.dot(weights) + bias - y) ** 2)
