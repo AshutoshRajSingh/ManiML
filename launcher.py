b74131ea9e6937f2a60b4a485d59a1d2cc018f95
finite-simple-group-of-order-two <ashutosh200210@outlook.com>
2023-01-06 13:06:11 +0530

    create launcher.py

---
+++ launcher.py
@@ -0,0 +1,13 @@
+from scenes import linreg, logreg
+
+class LinregBatchGradientDescent(linreg.BatchGradientDescent):
+    pass
+
+class LinregStochasticGradientDescent(linreg.StochasticGradientDescent):
+    pass
+
+class LogregBatchGradientDescent(logreg.BatchGradientDescent):
+    pass
+
+class LogregStochasticGradientDescent(logreg.StochasticGradientDescent):
+    pass
